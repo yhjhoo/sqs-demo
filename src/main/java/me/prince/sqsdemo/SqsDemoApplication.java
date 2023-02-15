8f1b8328c51fbcab388ba132fc4d37a2b646ebd5
yhjhoo <yhjhoo@gmail.com>
2023-02-15 16:14:38 +0800

    init

---
+++ src/main/java/me/prince/sqsdemo/SqsDemoApplication.java
@@ -0,0 +1,16 @@
+package me.prince.sqsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsDemoApplication.class, args);
+	}
+
+}
+
+
+
---
+++ src/test/java/me/prince/sqsdemo/SqsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.prince.sqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
