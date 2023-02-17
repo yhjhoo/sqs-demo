2e0b4948533d3c9405989a824ebea3179dce6b8a
yhjhoo <yhjhoo@gmail.com>
2023-02-17 09:33:52 +0800

    test receive attr

--- src/main/java/me/prince/sqsdemo/SqsDemoApplication.java
+++ src/main/java/me/prince/sqsdemo/SqsDemoApplication.java
@@ -76,7 +76,8 @@ class MessageController {
 			ReceiveMessageRequest request = ReceiveMessageRequest.builder()
 					.queueUrl("https://sqs.ap-southeast-1.amazonaws.com/924307141432/hj_sqs.fifo")
 					.maxNumberOfMessages(10)
-					.attributeNamesWithStrings("a1")
+//					.attributeNamesWithStrings("a1")
+					.messageAttributeNames("a1")
 //					.attributeNames(QueueAttributeName.ALL, QueueAttributeName.valueOf("*"))
 					.waitTimeSeconds(20)
 					.build();
